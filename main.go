aa6c417c3ea24e757f782d3f2ad748bbf5fb1f0f
alex-arraga <alex-arraga@users.noreply.github.com>
2024-11-17 17:39:21 -0300

    library to read .env

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	fmt.Println("Hello RSS")
+
+	godotenv.Load(".env")
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		log.Fatal("PORT is not found")
+	}
+
+	fmt.Print("Port: ", port)
 }
