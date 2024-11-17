9f08de4798245101fee3c1649ab23fb825f655cc
alex-arraga <alex-arraga@users.noreply.github.com>
2024-11-17 18:10:52 -0300

    server init

--- main.go
+++ main.go
@@ -3,20 +3,36 @@ package main
 import (
 	"fmt"
 	"log"
+	"net/http"
 	"os"
 
+	"github.com/go-chi/chi"
 	"github.com/joho/godotenv"
 )
 
 func main() {
 	fmt.Println("Hello RSS")
 
+	// Export the variables of .env in the project
 	godotenv.Load(".env")
 
+	// Read env "PORT"
 	port := os.Getenv("PORT")
 	if port == "" {
 		log.Fatal("PORT is not found")
 	}
 
-	fmt.Print("Port: ", port)
+	// Create router and server
+	router := chi.NewRouter()
+
+	srv := &http.Server{
+		Handler: router,
+		Addr:    ":" + port,
+	}
+
+	log.Printf("Server starting on port: %s", port)
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
