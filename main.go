fc7a67570b3771fdf0cfd8a1e0b4320996151bec
alex-arraga <alex-arraga@users.noreply.github.com>
2024-11-19 18:03:27 -0300

    v1 router

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 	"github.com/joho/godotenv"
 )
 
@@ -24,6 +25,20 @@ func main() {
 
 	// Create router and server
 	router := chi.NewRouter()
+
+	router.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: false,
+		MaxAge:           300,
+	}))
+
+	v1Router := chi.NewRouter()
+	v1Router.HandleFunc("/healthz", handlerReadiness) // It firing with all request verbs
+
+	router.Mount("/v1", v1Router)
 
 	srv := &http.Server{
 		Handler: router,
