502c672762168a14aff0cd08f74fea0bdd510bbe
alex-arraga <alex-arraga@users.noreply.github.com>
2024-11-27 17:25:43 -0300

    get user by api_key

--- handler_user.go
+++ handler_user.go
@@ -6,13 +6,15 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/alex-arraga/rss_project/internal/auth"
 	"github.com/alex-arraga/rss_project/internal/database"
 	"github.com/google/uuid"
 )
 
 func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
-		Name string `json:"name"`
+		Name   string `json:"name"`
+		ApiKey string `json:"api_key"`
 	}
 	params := parameters{}
 
@@ -31,6 +33,22 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 	})
 	if err != nil {
 		respondWithError(w, 400, fmt.Sprintf("Couldn't create user: %v", err))
+		return
+	}
+
+	respondWithJSON(w, 201, databaseUserToUser(user))
+}
+
+func (apiCfg *apiConfig) handlerGetUserByAPIKey(w http.ResponseWriter, r *http.Request) {
+	apiKey, err := auth.GetAPIKey(r.Header)
+	if err != nil {
+		respondWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
+		return
+	}
+
+	user, err := apiCfg.DB.GetUserByAPIKey(r.Context(), apiKey)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't get user: %v", err))
 		return
 	}
 
