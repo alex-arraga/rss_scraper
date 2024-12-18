31c7cbb836ceb49e0249806a2bc2797322175149
alex-arraga <alex-arraga@users.noreply.github.com>
2024-12-18 12:13:21 -0300

    middleware_auth

--- internal/api/middlewares/middleware_auth.go
+++ internal/api/middlewares/middleware_auth.go
@@ -5,34 +5,32 @@ import (
 	"net/http"
 
 	"github.com/alex-arraga/rss_project/internal/auth"
-	database "github.com/alex-arraga/rss_project/internal/database/sqlc"
 	"github.com/alex-arraga/rss_project/internal/utils"
 )
 
 type MiddlewareConfig struct {
-	DB *database.Queries
+	AuthService *auth.AuthConfig
 }
 
-// type authedHandler func(http.ResponseWriter, *http.Request, database.User)
-
+// MiddlewareAuth auth the user and set user data in the context
 func (m *MiddlewareConfig) MiddlewareAuth(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+		// Extract ApiKey from header
 		apiKey, err := auth.ExtractAPIKey(r.Header)
 		if err != nil {
 			utils.RespondWithError(w, http.StatusForbidden, fmt.Sprintf("Auth error: %v", err))
 			return
 		}
 
-		user, err := m.DB.GetUserByAPIKey(r.Context(), apiKey)
+		// Auth user
+		user, err := m.AuthService.AuthenticateUser(r.Context(), apiKey)
 		if err != nil {
-			utils.RespondWithError(w, http.StatusBadRequest, fmt.Sprintf("Couldn't get user: %v", err))
+			utils.RespondWithError(w, http.StatusUnauthorized, err.Error())
 			return
 		}
 
-		ctx := r.Context()
-		ctx = AddUserToContext(ctx, user)
-		r = r.WithContext(ctx)
-
-		next(w, r)
+		// Add user in the context
+		ctx := auth.AddUserToContext(r.Context(), user)
+		next.ServeHTTP(w, r.WithContext(ctx))
 	}
 }
