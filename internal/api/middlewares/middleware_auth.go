440c5f5fbd4535cc1b0ea0b8c1e925c624646611
alex-arraga <alex-arraga@users.noreply.github.com>
2024-12-23 18:24:19 -0300

    wrapper middleware_auth

--- internal/api/middlewares/middleware_auth.go
+++ internal/api/middlewares/middleware_auth.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/alex-arraga/rss_project/internal/auth"
+	database "github.com/alex-arraga/rss_project/internal/database/sqlc"
 	"github.com/alex-arraga/rss_project/internal/utils"
 )
 
@@ -12,8 +13,10 @@ type MiddlewareConfig struct {
 	AuthService *auth.AuthService
 }
 
+type AuthedHandler func(w http.ResponseWriter, r *http.Request, user database.User)
+
 // MiddlewareAuth auth the user and set user data in the context
-func (m *MiddlewareConfig) MiddlewareAuth(next http.Handler) http.Handler {
+func (m *MiddlewareConfig) MiddlewareAuth(handler AuthedHandler) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		// Extract ApiKey from header
 		apiKey, err := auth.ExtractAPIKey(r.Header)
@@ -29,8 +32,7 @@ func (m *MiddlewareConfig) MiddlewareAuth(next http.Handler) http.Handler {
 			return
 		}
 
-		// Add user in the context
-		ctx := auth.AddUserToContext(r.Context(), user)
-		next.ServeHTTP(w, r.WithContext(ctx))
+		// Gives control to the handler, injecting to the user
+		handler(w, r, user)
 	})
 }
