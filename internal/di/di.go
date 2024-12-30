90c6e782d9f9fc261b011ac88a98f4723f909e76
alex-arraga <alex-arraga@users.noreply.github.com>
2024-12-30 20:20:51 -0300

    auth service included in container

--- internal/di/di.go
+++ internal/di/di.go
@@ -1,22 +1,26 @@
 package di
 
 import (
+	"github.com/alex-arraga/rss_project/internal/auth"
 	database "github.com/alex-arraga/rss_project/internal/database/sqlc"
 	"github.com/alex-arraga/rss_project/internal/services"
 )
 
 type Container struct {
+	AuthSerive        *auth.AuthService
 	UserService       *services.UserService
 	FeedService       *services.FeedService
 	FeedFollowService *services.FeedFollowService
 }
 
 func NewContainer(db *database.Queries) (*Container, error) {
+	authService := &auth.AuthService{DB: db}
 	userService := &services.UserService{DB: db}
 	feedService := &services.FeedService{DB: db}
 	feedFollowService := &services.FeedFollowService{DB: db}
 
 	return &Container{
+		AuthSerive:        authService,
 		UserService:       userService,
 		FeedService:       feedService,
 		FeedFollowService: feedFollowService,
