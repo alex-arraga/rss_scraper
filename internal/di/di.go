4d3b731030ec3f8c1476697f69d535ad5001b622
alex-arraga <alex-arraga@users.noreply.github.com>
2024-12-30 18:49:09 -0300

    wip

--- internal/di/di.go
+++ internal/di/di.go
@@ -8,14 +8,17 @@ import (
 type Container struct {
 	UserService *services.UserService
 	FeedService *services.FeedService
+	FeedFollowService *services.FeedFollowService
 }
 
 func NewContainer(db *database.Queries) (*Container, error) {
 	userService := &services.UserService{DB: db}
 	feedService := &services.FeedService{DB: db}
+	feedFollowService := &services.FeedFollowService{DB: db}
 
 	return &Container{
 		UserService: userService,
 		FeedService: feedService,
+		FeedFollowService: feedFollowService,
 	}, nil
 }
